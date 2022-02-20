b70284126d9993f46f91b256d623cc50da424ab7
joe <joe.zhangyuechen@hotmail.com>
2022-02-19 23:08:39 -0500

    starting stage for translateService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web_portfolio';
+  
+  language = "ENG";
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+  }
+
+  changeLanguage() {
+    if (this.translate.currentLang === "en"){
+      this.translate.use('zh');
+      this.language = "中文";
+    }else{
+      this.translate.use('en');
+      this.language = "ENG";
+    }
+  }
 }
